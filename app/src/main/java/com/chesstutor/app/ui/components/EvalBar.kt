336821b07127c982f87c237dae76c4821a0f4f95
Oghosa Benedict <bennykent21@gmail.com>
2026-09-18 10:30:51 +0100

    Increase EvalBar width from 22dp to 25dp

--- app/src/main/java/com/chesstutor/app/ui/components/EvalBar.kt
+++ app/src/main/java/com/chesstutor/app/ui/components/EvalBar.kt
@@ -104,7 +104,7 @@ fun EvalBar(
 
     Box(
         modifier = modifier
-            .width(22.dp)
+            .width(25.dp)
             .clip(RoundedCornerShape(3.dp))
             .background(Color(0xFF14181D)) // dark base = "black" side, always visible underneath
             .border(1.dp, ChessTutorColors.Line, RoundedCornerShape(3.dp))
